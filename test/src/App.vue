f1f3b8f6fc7f5b9233b757375cd7d124440b28bc
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-06-27 16:36:31 -0700

    add exports preview routing

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <h1 class="d-ta-center">Exported Components</h1>
-  <ul class="d-ls-reset">
-    <dt-list-item
+  <ul class="d-ls-reset d-ta-center">
+    <router-link
       v-for="(component) in Object.values(components)"
       :key="component.name"
-      navigation-type="tab"
-      role="menuitem"
+      :to="component.name"
     >
-      {{ component.name }}
-    </dt-list-item>
+      <dt-list-item
+        navigation-type="tab"
+        role="menuitem"
+      >
+        {{ component.name }}
+      </dt-list-item>
+    </router-link>
+
   </ul>
 </template>
 
 <script setup>
   import { DtListItem } from '@dialpad/dialtone-vue'
   import components from 'dialtone-sandbox';
+  import { RouterLink } from 'vue-router';
 </script>
 
 <style type="less" src="@/assets/dialtone.less" />
---
+++ test/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as VueRouter from 'vue-router';
+import components from 'dialtone-sandbox';
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+
+  routes: [
+    { path: '/', component: App },
+    ...Object.values(components).map(component => {
+      return { path: `/${component.name}`, component };
+    }),
+  ],
+});
+
+const app = createApp({});
+app.use(router);
+app.mount('#app');
