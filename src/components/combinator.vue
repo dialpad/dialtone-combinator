e511ee13a2208cacd9f5f16530bb6440a1b827d2
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-06-28 14:39:02 -0700

    feat(combinator): add sub components

--- src/components/combinator.vue
+++ src/components/combinator.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="dc-root d-d-grid d-gg16 d-gl-sidebar d-h100p">
-    <div class="dc-root__sidebar d-ga-sidebar d-bgc-purple-200">...</div>
-    <div class="d-ga-content">
-      <div class="dc-root__body d-d-grid d-gg16 d-h100p">
-        <div class="dc-root__top d-bgc-green-200">...</div>
-        <div class="dc-root__bottom d-bgc-orange-200">...</div>
+    <div class="dc-root__body d-d-grid d-gg16 d-ga-content d-h100p">
+      <div class="dc-root__top d-bgc-green-200">
+        <dc-renderer />
       </div>
+      <div class="dc-root__bottom d-bgc-orange-200">
+        <dc-code-preview />
+      </div>
+    </div>
+    <div class="d-ga-sidebar d-bgc-purple-200">
+      <dc-option-bar />
     </div>
   </div>
 </template>
 
 <script>
+import DcRenderer from '@/components/renderer.vue';
+import DcOptionBar from '@/components/option_bar.vue';
+import DcCodePreview from '@/components/code_preview.vue';
+
 export default {
   name: 'DcCombinator',
+  components: {
+    DcRenderer,
+    DcOptionBar,
+    DcCodePreview,
+  },
 };
 </script>
+
+<style lang="less">
+.dc-root {
+  direction: rtl;
+}
+
+.dc-root > * {
+  direction: initial;
+}
+</style>
