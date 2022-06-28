2d86626fbdd23a03fcf4213921523adfb7d0787b
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-06-28 12:02:42 -0700

    add eslint package

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'standard',
+    'semistandard',
+    'plugin:vue/vue3-recommended',
+    'plugin:vuejs-accessibility/recommended'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    amd: true,
+  },
+  rules: {
+  }
+}
