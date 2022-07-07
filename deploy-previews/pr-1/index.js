130641692839cda52848e8ecd42ffe17051beba1
JonathanPaugh <JonathanPaugh@users.noreply.github.com>
2022-07-07 02:10:13 +0000

    Deploying to gh-pages from @ dialpad/dialtone-combinator@4dd04fa5acc3c181d3a5ccbd00a9ffee103e0632 🚀

--- deploy-previews/pr-1/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DtcTestBase } from './src/components/test/test_base.vue';
-export { default as DtcTestDialtone } from './src/components/test/test_dialtone.vue';
-export { default as DtcTestDialtoneVue } from './src/components/test/test_dialtone_vue.vue';
-export { default as DtcCombinator } from './src/components/combinator.vue';
