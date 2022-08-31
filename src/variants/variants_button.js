dfb48461c91ae7ec90e04a553d36604837efaf1b
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-08-30 17:09:17 -0700

    feat: add more button variants

--- src/variants/variants_button.js
+++ src/variants/variants_button.js
@@ -6,4 +6,108 @@ export default {
       },
     },
   },
+  primary: {
+    slots: {
+      default: {
+        initialValue: 'Primary',
+      },
+    },
+    props: {
+      importance: {
+        initialValue: 'primary',
+        lockControl: true,
+      },
+    },
+  },
+  outlined: {
+    slots: {
+      default: {
+        initialValue: 'Outlined',
+      },
+    },
+    props: {
+      importance: {
+        initialValue: 'outlined',
+        lockControl: true,
+      },
+    },
+  },
+  clear: {
+    slots: {
+      default: {
+        initialValue: 'Clear',
+      },
+    },
+    props: {
+      importance: {
+        initialValue: 'clear',
+        lockControl: true,
+      },
+    },
+  },
+  danger: {
+    slots: {
+      default: {
+        initialValue: 'Danger',
+      },
+    },
+    props: {
+      kind: {
+        initialValue: 'danger',
+        lockControl: true,
+      },
+    },
+  },
+  inverted: {
+    slots: {
+      default: {
+        initialValue: 'Inverted',
+      },
+    },
+    props: {
+      kind: {
+        initialValue: 'inverted',
+        lockControl: true,
+      },
+    },
+  },
+  disabled: {
+    slots: {
+      default: {
+        initialValue: 'Disabled',
+      },
+    },
+    attributes: {
+      disabled: {
+        initialValue: true,
+        lockControl: true,
+      },
+    },
+  },
+  link: {
+    slots: {
+      default: {
+        initialValue: 'Link',
+      },
+    },
+    props: {
+      link: {
+        initialValue: true,
+        lockControl: true,
+      },
+    },
+  },
+  loading: {
+    slots: {
+      default: {
+        initialValue: 'Loading',
+      },
+    },
+    props: {
+      loading: {
+        initialValue: true,
+        lockControl: true,
+      },
+    },
+  },
 };
